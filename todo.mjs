66d7891698f0cc29ad4ddef78d6b78440efdca51
AhmedRaza <https://github.com/syedabdulbasitali12/new-azaan-app.git>
2022-10-03 19:50:14 +0500

    update

--- todo.mjs
+++ todo.mjs
@@ -1,9 +1,10 @@
 import express from 'express'
+import cors from 'cors';
 const app = express()
-const port = process.env.PORT || 3000;
-let todos= [];
+const port = process.env.PORT || 30000;
+let todos = [];
 app.use(express.json());
-//app.use(cors())
+app.use(cors())
 app.post('/todo', (req, res) => {
   console.log(req.body)
   todos.push(req.body.text);
