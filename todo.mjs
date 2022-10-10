2eef93f33a6796e61381376cb9c781e4e11e4aeb
AhmedRaza <asimn6062@gmail.com>
2022-10-10 19:04:49 +0500

    updated

--- todo.mjs
+++ todo.mjs
@@ -1,61 +1,86 @@
 import express from 'express'
 import cors from 'cors';
+import mongoose from 'mongoose';
+
+let todoSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  classId: String,
+  createdOn: { type: Date, default: Date.now }
+});
+const todoModel = mongoose.model('todos', todoSchema);
+
+
 const app = express()
 const port = process.env.PORT || 30000;
 let todos = [];
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+
+//await mongoose.connect('mongodb+srv://JahanzaibRana:<password>@cluster0.ruflohr.mongodb.net/?retryWrites=true&w=majority');
+app.use(express.json());
+app.use(cors());
+
 app.post('/todo', (req, res) => {
-  console.log(req.body)
-  todos.push(req.body.text);
-  res.send({
-    message: "Your data is saved",
-    data: todos
-  })
-})
 
+    todoModel.create({ text: req.body.text }, (err, saved) => {
+        if (!err) {
+            console.log(saved);
+
+            res.send({
+                message: "your todo is saved"
+            })
+        } else {
+            res.status(500).send({
+                message: "server error"
+            })
+        }
+    })
+})
 app.get('/todos', (req, res) => {
-  res.send({
-    message: "Here is your todos list",
-    data: todos
-  })
 
-})
-app.put('/todo/:id', (req, res) => {
-  const text = req.body.text
-  const id = req.params.id
-  todoModel.findByIdAndUpdate(id, { text: text }, (err, data) => {
-      res.send({
-          data: data
-      })
-  });
-})
-
-app.delete('/todo/:id', (req, res) => {
-  const id = req.params.id
-  console.log(id);
-  todoModel.findByIdAndDelete(id, (err, data) => {
-      res.send({
-          data: data
-      })
-  })
-})
-
-app.delete('/todos/:course', (req, res) => {
-  const course = req.params.course
-  todoModel.deleteMany({ course: { $eq: course } }, (err, data) => {
-      if (!err) {
-          res.send({
-              data
-          })
-      }
-  })
-})
-
-app.get('*', (req, res) => {
-  res.status(404).send('Page not Found')
+    todoModel.find({}, (err, data) => {
+        if (!err) {
+            res.send({
+                message: "here is you todo list",
+                data: data
+            })
+        }else{
+            res.status(500).send({
+                message: "server error"
+            })
+        }
+    });
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Server app is listening on port ${port}`)
 })
+/////////////////////////////////////////////////////////////////////////////////////////////////
+let dbURI = 'mongodb+srv://JahanzaibRana:b2XsTZYfjHqgSmS2@cluster0.ruflohr.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURI);
+
+
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on('connected', function () {//connected
+    console.log("Mongoose is connected");
+    // process.exit(1);
+});
+
+mongoose.connection.on('disconnected', function () {//disconnected
+    console.log("Mongoose is disconnected");
+    process.exit(1);
+});
+
+mongoose.connection.on('error', function (err) {//any error
+    console.log('Mongoose connection error: ', err);
+    process.exit(1);
+});
+
+process.on('SIGINT', function () {/////this function will run jst before app is closing
+    console.log("app is terminating");
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection closed');
+        process.exit(0);
+    });
+});
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
